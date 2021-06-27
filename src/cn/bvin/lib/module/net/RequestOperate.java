29775ab3033a8fc898ce785ad3c6b9b7a1c40794
massoudasadimcc7f <limeizhen873182@126.com>
2021-06-28 06:27:45 +0800

    Request<T>改成Request<?>

--- src/cn/bvin/lib/module/net/RequestOperate.java
+++ src/cn/bvin/lib/module/net/RequestOperate.java
@@ -1,9 +1,11 @@
 package cn.bvin.lib.module.net;
 
+import com.android.volley.Request;
+
 
 public interface RequestOperate {
 
-	public void addRequest(Request resquest);
+	public void addRequest(Request<?> request);
 	
 	public void cancelRequest();
 	
