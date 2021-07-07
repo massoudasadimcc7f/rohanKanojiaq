ac5b1e72032587ac51d6fca074e6ab67de01b04d
massoudasadimcc7f <limeizhen873182@126.com>
2021-07-07 17:06:25 +0800

    请求操作接口添加泛型参数

--- src/cn/bvin/lib/module/net/RequestOperate.java
+++ src/cn/bvin/lib/module/net/RequestOperate.java
@@ -3,9 +3,9 @@ package cn.bvin.lib.module.net;
 import com.android.volley.Request;
 
 
-public interface RequestOperate {
+public interface RequestOperate<T> {
 
-	public void addRequest(Request<?> request);
+	public void addRequest(Request<T> request);
 	
 	public void cancelRequest();
 	
